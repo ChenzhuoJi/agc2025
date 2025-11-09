aa9788efb7f87def1abee2acb0e9735147afdf98
姬晨卓 <3610396894@qq.com>
2025-11-09 22:36:34 +0800

    Ignore experiment and processing scripts directories

--- src/experiment_results_analysis/1.py
+++
@@ -1,3 +0,0 @@
-from src.helpers import paramsManager,dataStorageManager
-ds = dataStorageManager('data',p=2,theta=0.5)
-print(ds.file_to_save)
--- src/experiment_scripts/search.py
+++
@@ -1,4 +0,0 @@
-from src.experiment import Experiment, testExperiment, searchExperiment
-
-epr = searchExperiment("citeseer",sample_size=None)
-epr.run(pred_method="commutitude")
